6bc4b3d246a505e4c8089c422a883b8e601618b2
derekjanni <derekjanni@gmail.com>
2018-01-16 20:36:45 -0800

    Update cache.py

--- database/cache.py
+++ database/cache.py
@@ -1,14 +1,3 @@
-#
-# CONFIDENTIAL
-#
-# This source code is proprietary, confidential information of Pulselocker, Inc.
-# It contains Pulselocker intellectual property, including trade secrets and
-# copyright-protected authorship, and may include patentable inventions. You may
-# not distribute this source code outside of Pulselocker without express written
-# permission from management. Pulselocker does not claim ownership of included
-# open source software components, which are subject to their own licenses.
-#
-
 import recommendations.config as config
 from pl_cache import RedisCache
 
